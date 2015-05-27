e8dc1a16090723ebaace15de44f3ff78fb5c8efb
mindyor <or.mindy@gmail.com>
2015-05-27 19:05:17 -0300

    java related things into its own file. source.

---
+++ files/java_specific.sh
@@ -0,0 +1,10 @@
+alias java_ls='/usr/libexec/java_home -V 2>&1 | grep -E "\d.\d.\d[,_]" | cut -d , -f 1 | colrm 1 4 | grep -v Home'
+
+# Switch java versions
+function java_use() {
+    export JAVA_HOME=$(/usr/libexec/java_home -v $1)
+    export PATH=$JAVA_HOME/bin:$PATH
+    java -version
+}
+java_use 1.6
+
