7b0be704b4e23ff2bd56552439679de13dc631ba
mindyor <or.mindy@gmail.com>
2015-08-26 15:41:41 -0700

    add alias for appium test command

--- files/work_related.sh
+++ files/work_related.sh
@@ -1,5 +1,6 @@
 alias gruntall="grunt lint && grunt test --once && grunt iso"
 alias gruntallall="gruntall && grunt fusion && grunt smoke"
 # don't forget appium & first
-alias runFT="gradle rFT -PTAGS=@appium -Pfeatures=features -PBROWSER=appium -PAPP_PATH=/Users/test/gap/snapscan/trunk/functional-tests/SnapScan-4.30837.30837-iphonesimulator.zip"
+# alias runFT="gradle rFT -PTAGS=@appium -Pfeatures=features -PBROWSER=appium -PAPP_PATH=/Users/test/gap/snapscan/trunk/functional-tests/SnapScan-4.30837.30837-iphonesimulator.zip"
+alias runFT="./gradlew rFT -PTAGS=@appium -Pfeatures=features -PBROWSER=appium -PAPP_PATH=/Users/test/Downloads/SnapScan.app -PAPP_ARGS=fusion2_ci"
 alias iso="(z OisF && grunt isolation:local)"
