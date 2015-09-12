9424544d4f083bbbc9e4caddbbad497a44520015
mindyor <or.mindy@gmail.com>
2015-09-12 01:42:22 -0700

    get rid of absolute paths

--- files/osx_install_apps.sh
+++ files/osx_install_apps.sh
@@ -1,2 +1,2 @@
-brew bundle check --file=/Users/mindyor/dotfiles/files/Brewfile
-brew bundle --file=/Users/mindyor/dotfiles/files/Brewfile
+brew bundle check --file=$HOME/dotfiles/files/Brewfile
+brew bundle --file=$HOME/dotfiles/files/Brewfile
