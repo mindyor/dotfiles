2ab598385a36d81558b28859d456e76de7797dd6
mindyor <or.mindy@gmail.com>
2019-03-29 14:59:45 -0700

    default to java 11

--- files/java_specific.sh
+++ files/java_specific.sh
@@ -1,10 +1,11 @@
 alias java_ls='/usr/libexec/java_home -V 2>&1 | grep -E "\d.\d.\d[,_]" | cut -d , -f 1 | colrm 1 4 | grep -v Home'
 
 # Switch java versions
+# 1.8, 11
 function java_use() {
     export JAVA_HOME=$(/usr/libexec/java_home -v $1)
     export PATH=$JAVA_HOME/bin:$PATH
     java -version
 }
-java_use 1.8
+java_use 11
 
