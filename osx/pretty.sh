a71e4ce375de2991cf758e650d6b45cb87b16b54
mindyor <or.mindy@gmail.com>
2019-04-02 23:08:20 -0700

    delete all apps in dock

--- osx/pretty.sh
+++ osx/pretty.sh
@@ -5,6 +5,9 @@ defaults write com.apple.Dock orientation -string right
 # dockbar small icons
 defaults write com.apple.Dock tilesize -int 24
 
+# delete all apps in dock
+#defaults delete com.apple.Dock persistent-apps
+
 # restart dock
 killall Dock
 
